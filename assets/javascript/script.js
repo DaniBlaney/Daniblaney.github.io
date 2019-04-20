8d11ead3914cfd5e859ee4ba987291edd71512f3
Danielle Blaney <daniblaney@gmail.com>
2019-04-19 22:24:16 -0400

    firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,53 +1,57 @@
-var video = document.querySelector('video')
-  , container = document.querySelector('#container');
-
-var setVideoDimensions = function () {
-  // Video's intrinsic dimensions
-  var w = video.videoWidth
-    , h = video.videoHeight;
-
-  // Intrinsic Ratio
-  // Will be more than 1 if W > H and less if W < H
-  var videoRatio = (w / h).toFixed(2);
-
-  // Get the container's computed styles
-  //
-  // Also calculate the min dimensions required (this will be
-  // the container dimentions)
-  var containerStyles = window.getComputedStyle(container)
-    , minW = parseInt( containerStyles.getPropertyValue('width') )
-    , minH = parseInt( containerStyles.getPropertyValue('height') );
-
-  // What's the min:intrinsic dimensions
-  //
-  // The idea is to get which of the container dimension
-  // has a higher value when compared with the equivalents
-  // of the video. Imagine a 1200x700 container and
-  // 1000x500 video. Then in order to find the right balance
-  // and do minimum scaling, we have to find the dimension
-  // with higher ratio.
-  //
-  // Ex: 1200/1000 = 1.2 and 700/500 = 1.4 - So it is best to
-  // scale 500 to 700 and then calculate what should be the
-  // right width. If we scale 1000 to 1200 then the height
-  // will become 600 proportionately.
-  var widthRatio = minW / w
-    , heightRatio = minH / h;
-
-  // Whichever ratio is more, the scaling
-  // has to be done over that dimension
-  if (widthRatio > heightRatio) {
-    var newWidth = minW;
-    var newHeight = Math.ceil( newWidth / videoRatio );
-  }
-  else {
-    var newHeight = minH;
-    var newWidth = Math.ceil( newHeight * videoRatio );
-  }
-
-  video.style.width = newWidth + 'px';
-  video.style.height = newHeight + 'px';
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD73sT69E7qtwjrRRG1mVn1_FRBSDHxb5Y",
+  authDomain: "profilegithubio.firebaseapp.com",
+  databaseURL: "https://profilegithubio.firebaseio.com",
+  projectId: "profilegithubio",
+  storageBucket: "profilegithubio.appspot.com",
+  messagingSenderId: "222453668265"
 };
+firebase.initializeApp(config);
 
-video.addEventListener('loadedmetadata', setVideoDimensions, false);
-window.addEventListener('resize', setVideoDimensions, false);
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  //Get value
+  var name = getInputVal('name');
+
+  var email = getInputVal('email');
+
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get form value
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    message: message
+  });
+}
