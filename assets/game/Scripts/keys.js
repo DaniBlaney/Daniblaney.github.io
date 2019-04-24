072a97062bf55d151c57cfb2a8a832e80f5b6543
Danielle Blaney <daniblaney@gmail.com>
2019-04-23 23:17:07 -0400

    started touch controls

--- assets/game/Scripts/keys.js
+++ assets/game/Scripts/keys.js
@@ -59,31 +59,65 @@ var keys = {
 	down : false,
 };
 
+/*------------------------------------------------------------------------------*/
 /* make mobile touch screen controls--my code added to Florians's*/
 
-var myGameArea = {
-  canvas : document.createElement("canvas"),
-  start : function() {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-    window.addEventListener('touchmove', function (e) {
-      myGameArea.x = e.touches[0].screenX;
-      myGameArea.y = e.touches[0].screenY;
-    })
-  },
-  clear : function(){
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-}
+// var el = document.getElementsByTagName("canvas")[0];
+// el.addEventListener("touchstart", handleStart);
+// el.addEventListener("touchmove", handleMove);
+// el.addEventListener("touchend", handleEnd);
+// el.addEventListener("touchcancel", handleCancel);
 
-function updateGameArea() {
-  myGameArea.clear();
-  if (myGameArea.x && myGameArea.y) {
-    myGamePiece.x = myGameArea.x;
-    myGamePiece.y = myGameArea.y;
-  }
-  myGamePiece.update();
-}
+var gameController {
+	bind : function() {
+		$(document).on('ontouchstart', function(event) {
+			return touches.handler(event, true);
+		});
+		$(document).on('ontouchend', function(event) {
+			return touches.handler(event, false);
+		});
+	},
+	reset : function() {
+		keys.left = false;
+		keys.right = false;
+		keys.accelerate = false; //hold
+		keys.up = false; //a button
+		keys.down = false; //b button
+	},
+	unbind : function() {
+		$(document).off('ontouchstart');
+		$(document).off('ontouchend');
+	},
+
+	touchHandler : function(event, status) {
+		switch(event.keyCode) {
+			case 57392://CTRL on MAC
+			case 17://CTRL
+			case 65://A
+				keys.accelerate = status;
+				break;
+			case 40://DOWN ARROW
+				keys.down = status;
+				break;
+			case 39://RIGHT ARROW
+				keys.right = status;
+				break;
+			case 37://LEFT ARROW
+				keys.left = status;
+				break;
+			case 38://UP ARROW
+				keys.up = status;
+				break;
+			default:
+				return true;
+		}
+
+	event.preventDefault();
+	return false;
+},
+accelerate : false,
+left : false,
+up : false,
+right : false,
+down : false,
+};
