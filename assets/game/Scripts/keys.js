ae40b8883852d37462f770b1c943c6d7812ff322
Danielle Blaney <daniblaney@gmail.com>
2019-04-24 10:32:31 -0400

    added images for controller

--- assets/game/Scripts/keys.js
+++ assets/game/Scripts/keys.js
@@ -68,56 +68,56 @@ var keys = {
 // el.addEventListener("touchend", handleEnd);
 // el.addEventListener("touchcancel", handleCancel);
 
-var gameController {
-	bind : function() {
-		$(document).on('ontouchstart', function(event) {
-			return touches.handler(event, true);
-		});
-		$(document).on('ontouchend', function(event) {
-			return touches.handler(event, false);
-		});
-	},
-	reset : function() {
-		keys.left = false;
-		keys.right = false;
-		keys.accelerate = false; //hold
-		keys.up = false; //a button
-		keys.down = false; //b button
-	},
-	unbind : function() {
-		$(document).off('ontouchstart');
-		$(document).off('ontouchend');
-	},
+// var gameController {
+// 	bind : function() {
+// 		$(document).on('ontouchstart', function(event) {
+// 			return touches.handler(event, true);
+// 		});
+// 		$(document).on('ontouchend', function(event) {
+// 			return touches.handler(event, false);
+// 		});
+// 	},
+// 	reset : function() {
+// 		keys.left = false;
+// 		keys.right = false;
+// 		keys.accelerate = false; //hold
+// 		keys.up = false; //a button
+// 		keys.down = false; //b button
+// 	},
+// 	unbind : function() {
+// 		$(document).off('ontouchstart');
+// 		$(document).off('ontouchend');
+// 	},
 
-	touchHandler : function(event, status) {
-		switch(event.keyCode) {
-			case 57392://CTRL on MAC
-			case 17://CTRL
-			case 65://A
-				keys.accelerate = status;
-				break;
-			case 40://DOWN ARROW
-				keys.down = status;
-				break;
-			case 39://RIGHT ARROW
-				keys.right = status;
-				break;
-			case 37://LEFT ARROW
-				keys.left = status;
-				break;
-			case 38://UP ARROW
-				keys.up = status;
-				break;
-			default:
-				return true;
-		}
+// 	touchHandler : function(event, status) {
+// 		switch(event.keyCode) {
+// 			case 57392://CTRL on MAC
+// 			case 17://CTRL
+// 			case 65://A
+// 				keys.accelerate = status;
+// 				break;
+// 			case 40://DOWN ARROW
+// 				keys.down = status;
+// 				break;
+// 			case 39://RIGHT ARROW
+// 				keys.right = status;
+// 				break;
+// 			case 37://LEFT ARROW
+// 				keys.left = status;
+// 				break;
+// 			case 38://UP ARROW
+// 				keys.up = status;
+// 				break;
+// 			default:
+// 				return true;
+// 		}
 
-	event.preventDefault();
-	return false;
-},
-accelerate : false,
-left : false,
-up : false,
-right : false,
-down : false,
-};
+// 	event.preventDefault();
+// 	return false;
+// },
+// accelerate : false,
+// left : false,
+// up : false,
+// right : false,
+// down : false,
+// };
