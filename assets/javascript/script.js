c3545de72d00c2a4501bd7d1634a4ca659106818
DaniBlaney <daniblaney@gmail.com>
2019-06-22 16:54:39 -0400

    480

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
 
 // Initialize Firebase
   var config = {
